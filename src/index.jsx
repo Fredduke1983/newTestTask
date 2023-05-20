5ab56dc90a3e761c01933790c19d7867a3f7f52e
Nechai_Ihor <fredduke1983@gmail.com>
2023-05-20 14:45:50 +0300

    up

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/testTask">
+    <BrowserRouter basename="/newtesttask">
       <App />
     </BrowserRouter>
   </React.StrictMode>
